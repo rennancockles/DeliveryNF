954b047cb38541541999111fba2728b433d95eaa
Rennan Cockles <rennancockles@gmail.com>
2020-03-25 03:45:59 -0300

    google analytics

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+    id: 'UA-xxxxxxxxx-x',
+    router
+})
 
 new Vue({
     router,
